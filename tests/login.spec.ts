8c991c050c67aac0de20e0c91123590ce84de93f
Jagadeesh <vedulajagadeesh1994@gmail.com>
2026-06-07 10:18:55 +0530

    WA-2 Create Login Page Automation flow

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -6,7 +6,7 @@ test.beforeEach("launch browser with proper url",async({page})=>{
   const base = new basepage(page);
   await base.navigate("/")
 })
-test("valid login scenario", async({ page })=>{
+test("login scenario", async({ page })=>{
   const Login = new loginPage(page);
   await Login.click_on_sigin();
   await Login.login_with_valid_cred("vedulajagadeesh1994@gmail.com","Yuvraj@12")
