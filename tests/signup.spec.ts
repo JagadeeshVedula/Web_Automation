870585a105a8952fd488672f47c3c46d56d8b9e1
Jagadeesh <vedulajagadeesh1994@gmail.com>
2026-06-08 07:05:17 +0530

    test data logic update

--- tests/signup.spec.ts
+++ tests/signup.spec.ts
@@ -2,6 +2,7 @@ import {test,expect} from '@playwright/test'
 import {signup} from '../src/pages/sign_up'
 import {loginPage} from '../src/pages/login_page'
 import {basepage} from '../src/pages/base_page'
+import testdata from '../Data/CreateAccount.json'
 
 test.beforeEach("Open Browser and Navigate to signup page",async({page})=>
 {
@@ -15,19 +16,5 @@ test.beforeEach("Open Browser and Navigate to signup page",async({page})=>
 test("create user account",async({page})=>
 {
     const createAccount = new signup(page)
-    await createAccount.create_Account({
-        Name:"Yuvraj",
-        Email:"Yuvraj54564@gmail.com",
-        Title: "Mr",  //Mr or Mrs
-        Password:"Yuvraj@12",
-        DOB:"01-08-1994", //DD-MONTH-YEAR format
-        FirstName:"Yuvi",
-        LastName:"Raj",
-        Address:"Narsipatnam",
-        Country:"India",  // India format
-        State:"Andhra Pradesh",
-        City:"Visakhapatnam",
-        Zipcode:"531116",
-        Mobile:"118484484"
-    })
+    await createAccount.create_Account(testdata)
 })
