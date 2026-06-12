9ef8f297c93130acbc3349949e9a6afdc2ea1dd7
Jagadeesh <vedulajagadeesh1994@gmail.com>
2026-06-12 07:03:49 +0530

    Added fixtures

--- tests/signup.spec.ts
+++ tests/signup.spec.ts
@@ -1,23 +1,17 @@
-import {test,expect} from '@playwright/test'
-import {signup} from '../src/pages/sign_up'
-import {loginPage} from '../src/pages/login_page'
-import {basepage} from '../src/pages/base_page'
+import {test} from '../src/fixtures/basefixture'
 import testdata from '../Data/CreateAccount.json'
 
 test.describe("register",{tag:["@signup", "@regression"]},()=>{
-    test.beforeEach("Open Browser and Navigate to signup page",async({page})=>
+    test.beforeEach("Open Browser and Navigate to signup page",async({Base,Login})=>
     {
-        const base = new basepage(page);
-        await base.navigate("/")
-        const login = new loginPage(page)
-        await login.click_on_sigin()
+        await Base.navigate("/")
+        await Login.click_on_sigin()
     })
 
 
-    test("create user account",async({page})=>
+    test("create user account",async({SignUp})=>
     {
-        const createAccount = new signup(page)
-        await createAccount.create_Account(testdata)
+        await SignUp.create_Account(testdata)
     })
 
 })
